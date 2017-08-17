53a69bb1890b8334cc0a90b19ab83c9ca4dd6530
Roushan <roushan.jha0709@gmail.com>
2017-08-17 19:10:33 +0530

    adde DEV server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema.js');
+
+
+const app = express();
+
+app.use('/graphql', expressGraphQL({
+     
+     schema:schema,
+     graphiql:true
+
+}));
+
+
+app.listen(process.env.PORT || 3000, function() {
+
+	console.log('server is listening');
+});
