ea412c728eb8299e59a28c720087df0593292e74
Roushan <roushan.jha0709@gmail.com>
2017-08-19 19:37:02 +0530

    connected db to mlabs

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const expressGraphQL = require('express-graphql');
-const schema = require('./schema.js');
+const mongoose = require('mongoose');
+const schema = require('./schema/schema.js');
 
 
 const app = express();
+
+mongoose.connect('mongodb://oliver:oliver@ds151963.mlab.com:51963/graphql')
+const db = mongoose.connection;
+
+db.on('error', () => console.log('Failed'))
+   .once('open', () => console.log('connected'))
 
 app.use('/graphql', expressGraphQL({
      
